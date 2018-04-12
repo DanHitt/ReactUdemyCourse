c7a4118a854c39481efc7a6833310491cc2cc534
Dan Hitt <d.hitt424@gmail.com>
2018-04-12 16:14:19 -0600

    change IndecisionApp to ES6
    word.

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -8,56 +8,23 @@ import Options from './Options';
 
 
  export default class IndecisionApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-        this.handlePick = this.handlePick.bind(this);
-        this.handleAddOption = this.handleAddOption.bind(this);
-        this.handleDeleteOption = this.handleDeleteOption.bind(this);
-        this.state = {options: props.options};
-   
-    }
-    componentDidMount() {
-        try {
-            const json = localStorage.getItem('options');
-            const options = JSON.parse(json);
-    
-            if (options) {
-                this.setState(() => ({ options }));
-            }
-
-        } catch (e) {
-            //do nothing
-        }
-
-
-    }
-    componentDidUpdate(prevProps, prevState){
-        if (prevState.options.length !== this.state.options.length) {
-            const json = JSON.stringify(this.state.options);
-            localStorage.setItem('options', json);            
-        }
-     
-    }
-
-    componentWillUpmount() {
-
-    }
-
-    handleDeleteOptions() {
+     state = {
+        options: []
+     };
+     handleDeleteOptions = () => {
         this.setState(() => ({options: []}))
-    }
-    handleDeleteOption(optionToRemove) {
+    };
+    handleDeleteOption = (optionToRemove) => {
         this.setState((prevState) => ({
             options: prevState.options.filter((option) => optionToRemove !== option)
         }));
-    }
-    handlePick() {
+    };
+    handlePick = () => {
         const randomNum = Math.floor(Math.random() * this.state.options.length);
         const option = this.state.options[randomNum];
         alert(option);
-    }
-    handleAddOption(option) {
+    };
+    handleAddOption = (option) => {
         if (!option) {
             return 'Enter valid value to add item';
         } else if (this.state.options.indexOf(option) > -1) {
@@ -83,7 +50,36 @@ import Options from './Options';
                 <AddOption handleAddOption={this.handleAddOption}/>
             </div>
         );
+    };
+
+    componentDidMount() {
+        try {
+            const json = localStorage.getItem('options');
+            const options = JSON.parse(json);
+    
+            if (options) {
+                this.setState(() => ({ options }));
+            }
+
+        } catch (e) {
+            //do nothing
+        }
+
+
     }
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.options.length !== this.state.options.length) {
+            const json = JSON.stringify(this.state.options);
+            localStorage.setItem('options', json);            
+        }
+     
+    }
+
+    componentWillUpmount() {
+
+    }
+
+
 }
 IndecisionApp.defaultProps = {
     options: []
