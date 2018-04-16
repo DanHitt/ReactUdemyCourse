f639eb851cd9eec9d5fd6888e566c9c4053859d1
Dan Hitt <d.hitt424@gmail.com>
2018-04-16 13:04:45 -0600

    last of scss/sass

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,10 +6,12 @@ const OptionModal = (props) => (
         isOpen={!!props.selectedOption}
         onRequestClose={props.handleModalOkay}
         contentLabel="Selected Option"
+        closeTimeoutMS={200}
+        className="modal" // use this to replace all the default style on modal
     >
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleModalOkay}> Okay </button>
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.handleModalOkay}> Okay </button>
     </Modal>
 );
 
